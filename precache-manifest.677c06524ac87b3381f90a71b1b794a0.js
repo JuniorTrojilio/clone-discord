5fb18f483cbb27ede1f5f16d61264d7aa8507715
Junior Trojilio <osmar.trojilio@gmail.com>
2020-09-17 21:48:47 -0300

    Updates

---
+++ precache-manifest.677c06524ac87b3381f90a71b1b794a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10066f0e5bd6e0bed511d290886f856",
+    "url": "/clone-discord/index.html"
+  },
+  {
+    "revision": "8f716e0948216bcd09eb",
+    "url": "/clone-discord/static/js/2.47a60f28.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clone-discord/static/js/2.47a60f28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af40f9416dabd725d2ea",
+    "url": "/clone-discord/static/js/main.327a1439.chunk.js"
+  },
+  {
+    "revision": "42f72716eb42617ccaa7",
+    "url": "/clone-discord/static/js/runtime-main.2ab55999.js"
+  },
+  {
+    "revision": "4ab8569a9650b6628771bad82d6e2275",
+    "url": "/clone-discord/static/media/logo.4ab8569a.svg"
+  }
+]);
